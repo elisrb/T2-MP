a3016f7cee15fc59e5474335666fc55a49543f4b
elisrb <elisrb2004@gmail.com>
2025-01-14 17:45:53 -0300

    realiza testes de verificação

--- conta_palavras.cpp
+++ conta_palavras.cpp
@@ -18,9 +18,9 @@ using namespace std;
 wstring_convert<codecvt_utf8<wchar_t>> converter;
 
 /** 
- * @brief 
- * @param  
- * @return
+ * @brief Essa função abre um arquivo .txt e lê o conteúdo
+ * @param  str  path do arquivo .txt a ser aberto relativa ao diretório do código-fonte
+ * @return str  o conteúdo do arquivo (substituindo quebras de linha por espaços)
  */
 string read_file(string arquivo) {
     ifstream file(arquivo);
@@ -38,9 +38,9 @@ string read_file(string arquivo) {
 }
 
 /** 
- * @brief 
- * @param  
- * @return
+ * @brief Essa função separa as palavras de uma string por espaço
+ * @param  str  string de palavras a serem separadas
+ * @return vector<wstring>  lista em que cada item é uma palavra (convertida para wstring para facilitar a execução das outras funções)
  */
 vector<wstring> split_string(string texto) {
     int pos = 0;
@@ -65,9 +65,9 @@ vector<wstring> split_string(string texto) {
 }
 
 /** 
- * @brief 
- * @param  
- * @return
+ * @brief Essa função remove o acento dos caracteres para facilitar a ordenação
+ * @param  wchar_t  caractere a ser normalizado
+ * @return wchar_t  caso o caractere original seja acentuado, retorna o caractere com o acento removido; caso não, retorna o mesmo caractere
  */
 wchar_t remove_acento(wchar_t c) {
     if (c == L'ã' || c == L'á' || c == L'â' || c == L'à') return L'a';
@@ -80,9 +80,10 @@ wchar_t remove_acento(wchar_t c) {
 }
 
 /** 
- * @brief 
- * @param  
- * @return
+ * @brief Essa função é um comparador para a ordenação de palavras em ordem alfabética considerando maiúsculas e minúsculas e acentuação.
+ * Duas palavras com as mesmas letras (maiúsculas ou minúsculas, acentuadas ou não), aparecerão na ordem em que foram recebidas.
+ * @param  wstring  duas palavras (a e b) a serem comparadas
+ * @return  bool    true se a deve vir antes de b na ordenação, false se a deve vir depois de b
  */
 bool comp(wstring a, wstring b) {
     wstring comp_a = L"", comp_b = L"";
@@ -96,9 +97,10 @@ bool comp(wstring a, wstring b) {
 }
 
 /** 
- * @brief 
- * @param  
- * @return
+ * @brief Essa função formata a resposta do problema apresentado no roteiro do trabalho, estruturando a contagem das palavras.
+ * @param  vector<pair<wstring, int>>, vector<int>  o primeiro vetor é composto por pairs que contêm cada palavra do texto
+ * associada ao seu índice na lista da contagem de palavras, e outro vetor que é a lista da contagem de ocorrências de cada palavra
+ * @return wstring  a string da resposta formatada como "{palavra}: {número de ocorrências}\n", para cada palavra
  */
 wstring format(vector<pair<wstring, int>> palavras_contadas, vector<int> numero_palavras) {
     wstring ans = L"";
@@ -117,8 +119,8 @@ wstring format(vector<pair<wstring, int>> palavras_contadas, vector<int> numero_
 
 /** 
  * @brief Essa é a função principal do código, que chama todas as outras funções. Ela conta as ocorrências de cada palavra em um arquivo .txt fornecido.
- * @param  
- * @return 
+ * @param str   path do arquivo .txt a ser aberto relativa ao diretório do código-fonte
+ * @return wstring  a string da resposta formatada como "{palavra}: {número de ocorrências}\n", para cada palavra
  */
 wstring VerificaPalavras(string arquivo) {
     string file_contents = read_file(arquivo);
