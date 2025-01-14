07fac98bfd0eec9d651d74f466ff1698a91d99e7
elisrb <elisrb2004@gmail.com>
2025-01-14 18:48:20 -0300

    adiciona assertivas de entrada e saída para todas as funções

--- conta_palavras.cpp
+++ conta_palavras.cpp
@@ -20,7 +20,14 @@ wstring_convert<codecvt_utf8<wchar_t>> converter;
 /** 
  * @brief Essa função abre um arquivo .txt e lê o conteúdo dele
  * @param  str  path do arquivo .txt a ser aberto relativa ao diretório do código-fonte
- * @return str  o conteúdo do arquivo (substituindo quebras de linha por espaços)
+ * Assertivas de entrada:
+ * - a path do arquivo deve existir
+ * - o arquivo deve ser um .txt
+ * - não devem existir dois arquivos com o mesmo nome
+ * Assertivas de saída:
+ * - string contendo todo o conteúdo do arquivo
+ * - as quebras de linha são substituídas por espaços
+ * @return str  o conteúdo do arquivo
  */
 string read_file(string arquivo) {
     ifstream file(arquivo);
@@ -40,6 +47,11 @@ string read_file(string arquivo) {
 /** 
  * @brief Essa função separa as palavras de uma string por espaço
  * @param  str  string de palavras a serem separadas
+ * Assertivas de entrada:
+ * - string contendo várias palavras separadas apenas por espaços
+ * - as palavras podem ser separadas por mais de um espaço
+ * Assertivas de saída:
+ * - vetor de wstring em que cada item é uma palavra do texto fornecido
  * @return vector<wstring>  lista em que cada item é uma palavra (convertida para wstring para facilitar a execução das outras funções)
  */
 vector<wstring> split_string(string texto) {
@@ -65,8 +77,14 @@ vector<wstring> split_string(string texto) {
 }
 
 /** 
- * @brief Essa função remove o acento dos caracteres para facilitar a ordenação
+ * @brief Essa função remove o acento dos caracteres para facilitar a comparação e ordenação
  * @param  wchar_t  caractere a ser normalizado
+ * Assertivas de entrada:
+ * - o caractere deve ter sido previamente convertido para wchar_t
+ * - o caractere deve ser uma letra minúscula do alfabeto
+ * - os caracteres acentuados considerados são: ã, á, â, à, é, ê, í, õ, ó, ô, ú e ñ
+ * Assertivas de saída:
+ * - um wchar_t correspondente ao original sem acento
  * @return wchar_t  caso o caractere original seja acentuado, retorna o caractere com o acento removido; caso não, retorna o mesmo caractere
  */
 wchar_t remove_acento(wchar_t c) {
@@ -83,6 +101,10 @@ wchar_t remove_acento(wchar_t c) {
  * @brief Essa função é um comparador para a ordenação de palavras em ordem alfabética considerando maiúsculas e minúsculas e acentuação.
  * Duas palavras com as mesmas letras (maiúsculas ou minúsculas, acentuadas ou não), aparecerão na ordem em que foram recebidas.
  * @param  wstring  duas palavras (a e b) a serem comparadas
+ * Assertivas de entrada:
+ * - as palavras devem ter sido previamente convertidas para wstring
+ * Assertivas de saída:
+ * - as palavras serão ordenadas em ordem alfabética, mantendo os caracteres originais
  * @return  bool    true se a deve vir antes de b na ordenação, false se a deve vir depois de b
  */
 bool comp(wstring a, wstring b) {
@@ -100,7 +122,13 @@ bool comp(wstring a, wstring b) {
  * @brief Essa função formata a resposta do problema apresentado no roteiro do trabalho, estruturando a contagem das palavras.
  * @param  vector<pair<wstring, int>>, vector<int>  o primeiro vetor é composto por pairs que contêm cada palavra do texto
  * associada ao seu índice na lista da contagem de palavras, e outro vetor que é a lista da contagem de ocorrências de cada palavra
- * @return wstring  a string da resposta formatada como "{palavra}: {número de ocorrências}\n", para cada palavra
+ * Assertivas de entrada:
+ * - o vector<pair<wstring, int>> deve contemplar todas as palavras que foram contabilizadas
+ * - os pares desse vetor são compostos por uma wstring (a palavra) e um número inteiro correspondente ao índice dessa palavra no vetor
+ * o vector<int> deve conter o número correspondente à quantidade de ocorrências de cada palavra do primeiro vetor, no mesmo índice
+ * Assertivas de saída:
+ * - a string contém todas as palavras e o número de ocorrências, formatadas como "{palavra}: {número de ocorrências}\n", para cada palavra
+ * @return wstring  a string da resposta formatada
  */
 wstring format(vector<pair<wstring, int>> palavras_contadas, vector<int> numero_palavras) {
     wstring ans = L"";
@@ -120,7 +148,13 @@ wstring format(vector<pair<wstring, int>> palavras_contadas, vector<int> numero_
 /** 
  * @brief Essa é a função principal do código, que chama todas as outras funções. Ela conta as ocorrências de cada palavra em um arquivo .txt fornecido.
  * @param str   path do arquivo .txt a ser aberto relativa ao diretório do código-fonte
- * @return wstring  a string da resposta formatada como "{palavra}: {número de ocorrências}\n", para cada palavra
+ * Assertivas de entrada:
+ * - a path do arquivo deve existir
+ * - o arquivo deve ser um .txt
+ * - não devem existir dois arquivos com o mesmo nome
+ * Assertivas de saída:
+ * - a string contém todas as palavras e o número de ocorrências, formatadas como "{palavra}: {número de ocorrências}\n", para cada palavra
+ * @return wstring  a string da resposta formatada
  */
 wstring VerificaPalavras(string arquivo) {
     string file_contents = read_file(arquivo);
